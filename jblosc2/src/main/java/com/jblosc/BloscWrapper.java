105da1cf1b96063d96b04e959bf0e890aa36dd80
Andrés Alted <andres.alted@gmail.com>
2017-05-24 13:52:45 +0200

    Moved to jblosc2 folder

---
+++ jblosc2/src/main/java/com/jblosc/BloscWrapper.java
@@ -0,0 +1,307 @@
+package com.jblosc;
+
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
+
+import com.jblosc.jna.BloscLibrary;
+import com.jblosc.jna.ContextCparams;
+import com.jblosc.jna.ContextDparams;
+import com.jblosc.jna.ContextDparams.ByReference;
+import com.jblosc.jna.Sheader;
+import com.jblosc.jna.Sparams;
+import com.sun.jna.NativeLong;
+import com.sun.jna.Pointer;
+import com.sun.jna.ptr.NativeLongByReference;
+import com.sun.jna.ptr.PointerByReference;
+
+/**
+ * Class that adds a more Java oriented programming vision to the JNA interface
+ * 
+ * @author aalted
+ *
+ */
+public class BloscWrapper {
+
+	public static final int OVERHEAD = 16;
+
+	/**
+	 * Call to the JNA blosc_init()
+	 */
+	public void init() {
+		BloscLibrary.blosc_init();
+	}
+
+	/**
+	 * Call to the JNA blosc_destroy()
+	 */
+	public void destroy() {
+		BloscLibrary.blosc_destroy();
+	}
+
+	/**
+	 * Call to the JNA blosc_set_nthreads
+	 * 
+	 * @param nthreads
+	 */
+	public void setNumThreads(int nthreads) {
+		BloscLibrary.blosc_set_nthreads(nthreads);
+	}
+
+	/**
+	 * call to the JNA blosc_get_nthreads
+	 * 
+	 * @return
+	 */
+	public int getNumThreads() {
+		return BloscLibrary.blosc_get_nthreads();
+	}
+
+	/**
+	 * Call the JNA blosc_list_compressors
+	 * 
+	 * @return
+	 */
+	public String listCompressors() {
+		return BloscLibrary.blosc_list_compressors();
+	}
+
+	/**
+	 * Call to the JNA blosc_set_compressor
+	 * 
+	 * @param compname
+	 */
+	public void setCompressor(String compname) {
+		BloscLibrary.blosc_set_compressor(compname);
+	}
+
+	/**
+	 * Call to the JNA blosc_get_compressor
+	 * 
+	 * @return
+	 */
+	public String getCompressor() {
+		return BloscLibrary.blosc_get_compressor();
+	}
+
+	/**
+	 * Call to the JNA blosc_compname_to_compcode
+	 * 
+	 * @param compname
+	 * @return
+	 */
+	public int compnameToCompcode(String compname) {
+		return BloscLibrary.blosc_compname_to_compcode(compname);
+	}
+
+	/**
+	 * Call to the JNA blosc_compcode_to_compname
+	 * 
+	 * @param compcode
+	 * @return
+	 */
+	public String compcodeToCompname(int compcode) {
+		PointerByReference ptr = new PointerByReference();
+		BloscLibrary.blosc_compcode_to_compname(compcode, ptr);
+		Pointer p = ptr.getValue();
+		return p.getString(0);
+	}
+
+	/**
+	 * Call to the JNA blosc_get_version_string
+	 * 
+	 * @return
+	 */
+	public String getVersionString() {
+		return BloscLibrary.blosc_get_version_string();
+	}
+
+	/**
+	 * Call to the JNA blosc_get_complib_info If compname is wrong then
+	 * unchecked IllegalArgumentException is thrown
+	 * 
+	 * @param compname
+	 * @return a 2 elements array: 0 -> complib, 1 -> version
+	 */
+	String[] getComplibInfo(String compname) {
+		PointerByReference ptrComplib = new PointerByReference();
+		PointerByReference ptrVersion = new PointerByReference();
+		int compcode = BloscLibrary.blosc_get_complib_info(compname, ptrComplib, ptrVersion);
+		if (compcode == -1) {
+			throw new IllegalArgumentException();
+		}
+		String[] result = new String[2];
+		result[0] = ptrComplib.getValue().getString(0);
+		result[1] = ptrVersion.getValue().getString(0);
+		return result;
+	}
+
+	/**
+	 * Call to the JNA blosc_free_resources throws an uncheked RuntimeException
+	 * if there are problems freeing resources
+	 */
+	public void freeResources() {
+		if (BloscLibrary.blosc_free_resources() == -1) {
+			throw new RuntimeException();
+		}
+	}
+
+	public BufferSizes cbufferSizes(Buffer cbuffer) {
+		NativeLongByReference nbytes = new NativeLongByReference();
+		NativeLongByReference cbytes = new NativeLongByReference();
+		NativeLongByReference blocksize = new NativeLongByReference();
+		BloscLibrary.blosc_cbuffer_sizes(cbuffer, nbytes, cbytes, blocksize);
+		BufferSizes bs = new BufferSizes(nbytes.getValue().longValue(), cbytes.getValue().longValue(),
+				blocksize.getValue().longValue());
+		return bs;
+	}
+
+	private void checkSizes(long srcLength, long destLength) {
+		if (srcLength > (Integer.MAX_VALUE - BloscWrapper.OVERHEAD)) {
+			throw new IllegalArgumentException("Source array is too large");
+		}
+		if (destLength < (srcLength + BloscWrapper.OVERHEAD)) {
+			throw new IllegalArgumentException("Dest array is not large enough.");
+		}
+	}
+
+	private void checkExit(int w) {
+		if (w == 0) {
+			throw new RuntimeException("Compressed size larger then dest length");
+		}
+		if (w == -1) {
+			throw new RuntimeException("Error compressing data");
+		}
+	}
+
+	public int compress(int compressionLevel, int shuffleType, int typeSize, ByteBuffer src, long srcLength,
+			ByteBuffer dest, long destLength) {
+		checkSizes(srcLength, destLength);
+		src.position(0);
+		dest.position(0);
+		src.order(ByteOrder.nativeOrder());
+		dest.order(ByteOrder.nativeOrder());
+		int w = BloscLibrary.blosc_compress(compressionLevel, shuffleType, new NativeLong(typeSize),
+				new NativeLong(srcLength), src, dest, new NativeLong(destLength));
+		checkExit(w);
+		return w;
+	}
+
+	public int decompress(Buffer src, Buffer dest, long destSize) {
+		src.position(0);
+		dest.position(0);
+		return BloscLibrary.blosc_decompress(src, dest, new NativeLong(destSize));
+	}
+
+	public int compressCtx(ContextCparams context, Buffer src, long srcLength, Buffer dest, long destLength) {
+		src.position(0);
+		dest.position(0);
+		PointerByReference byref = BloscLibrary.blosc2_create_cctx(context);
+		int w = BloscLibrary.blosc2_compress_ctx(byref, new NativeLong(srcLength), src, dest,
+				new NativeLong(destLength));
+		checkExit(w);
+		return w;
+	}
+
+	public int decompressCtx(ContextDparams context, Buffer src, Buffer dest, int destSize) {
+		src.position(0);
+		dest.position(0);
+		PointerByReference byref = BloscLibrary.blosc2_create_dctx(context);
+		return BloscLibrary.blosc2_decompress_ctx(byref, src, dest, new NativeLong(destSize));
+	}
+
+	public int getitemCtx(ContextDparams context, Buffer src, int start, int nitems, Buffer dest) {
+		PointerByReference byref = BloscLibrary.blosc2_create_dctx(context);
+		return BloscLibrary.blosc2_getitem_ctx(byref, src, start, nitems, dest);
+	}
+
+	public void cbufferMetainfo(Buffer cbuffer, NativeLongByReference typesize, IntBuffer flags) {
+		BloscLibrary.blosc_cbuffer_metainfo(cbuffer, typesize, flags);
+	}
+
+	public void cbufferVersions(Buffer cbuffer, IntBuffer version, IntBuffer versionlz) {
+		BloscLibrary.blosc_cbuffer_versions(cbuffer, version, versionlz);
+	}
+
+	public Buffer cbufferComplib(Buffer cbuffer) {
+		return BloscLibrary.blosc_cbuffer_complib(cbuffer);
+	}
+
+	public int getItem(Buffer src, int start, int nitems, Buffer dest) {
+		return BloscLibrary.blosc_getitem(src, start, nitems, dest);
+	}
+
+	public Sheader newSchunk(Sparams sparams) {
+		return BloscLibrary.blosc2_new_schunk(sparams);
+	}
+
+	public int appendBuffer(Sheader sheader, int typesize, int nbytes, Buffer src) {
+		NativeLong nl = BloscLibrary.blosc2_append_buffer(sheader, new NativeLong(typesize), new NativeLong(nbytes),
+				src);
+		return nl.intValue();
+	}
+
+	public int decompressChunk(Sheader sheader, long nchunk, Buffer dest, int nbytes) {
+		return BloscLibrary.blosc2_decompress_chunk(sheader, nchunk, dest, nbytes);
+	}
+
+	public int destroySchunk(Sheader sheader) {
+		return BloscLibrary.blosc2_destroy_schunk(sheader);
+	}
+
+	public int setDeltaRef(Sheader sheader, NativeLong nbytes, Buffer ref) {
+		return BloscLibrary.blosc2_set_delta_ref(sheader, nbytes, ref);
+	}
+
+	public Buffer packedAppendBuffer(Buffer packed, int typesize, long nbytes, Buffer src) {
+		return BloscLibrary.blosc2_packed_append_buffer(packed, new NativeLong(typesize), new NativeLong(nbytes), src);
+	}
+
+	public int packedDecompressChunk(Buffer packed, int nchunk, ByteBuffer dest) {
+		PointerByReference ptr = new PointerByReference();
+		int size = BloscLibrary.blosc2_packed_decompress_chunk(packed, nchunk, ptr);
+		Pointer p = ptr.getValue();
+		dest.put(p.getByteBuffer(0, size));
+		return size;
+	}
+
+	public Buffer packSchunk(Sheader sheader) {
+		return BloscLibrary.blosc2_pack_schunk(sheader);
+	}
+
+	public Sheader unpackSchunk(Buffer packed) {
+		return BloscLibrary.blosc2_unpack_schunk(packed);
+	}
+
+	public void freeCtx(ContextCparams context) {
+		com.jblosc.jna.ContextCparams.ByReference br = context.byReference();
+		PointerByReference ptrByRef = new PointerByReference();
+//		Pointer ptr = context.getPointer();
+		ptrByRef.setPointer(br.getPointer());
+		BloscLibrary.blosc_context bc = new BloscLibrary.blosc_context(context.getPointer());
+		BloscLibrary.blosc2_free_ctx(bc.getPointer());
+	}
+
+	public void freeCtx(ContextDparams context) {
+		ByReference br = context.byReference();
+		PointerByReference ptrByRef = new PointerByReference();
+		BloscLibrary.blosc_context bc = new BloscLibrary.blosc_context(context.getPointer());
+//		Pointer ptr = context.getPointer();
+		ptrByRef.setPointer(br.getPointer());
+		BloscLibrary.blosc2_free_ctx(bc.getPointer());
+	}
+
+	public int getBlocksize() {
+		return BloscLibrary.blosc_get_blocksize();
+	}
+
+	public void setBlocksize(long blocksize) {
+		BloscLibrary.blosc_set_blocksize(new NativeLong(blocksize));
+	}
+
+	public void setSchunk(Sheader schunk) {
+		BloscLibrary.blosc_set_schunk(schunk);
+	}
+
+}
---
+++ jblosc2/src/test/java/com/jblosc/BloscTest.java
@@ -0,0 +1,355 @@
+package com.jblosc;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+import org.junit.Test;
+
+import com.jblosc.jna.BloscLibrary;
+import com.jblosc.jna.ContextCparams;
+import com.jblosc.jna.ContextDparams;
+import com.jblosc.jna.Sheader;
+import com.jblosc.jna.Sparams;
+import com.sun.jna.Memory;
+import com.sun.jna.NativeLong;
+import com.sun.jna.ptr.NativeLongByReference;
+
+public class BloscTest {
+
+	@Test
+	public void testCompressDecompressJNA() {
+		int SIZE = 100 * 100 * 100;
+		float data[] = new float[SIZE];
+		for (int i = 0; i < SIZE; i++) {
+			data[i] = i * 2;
+		}
+		float data_out[] = new float[SIZE];
+		long isize = SIZE * 4;
+		Memory m = new Memory(isize);
+		m.write(0, data, 0, SIZE);
+		Memory m2 = new Memory(isize);
+		BloscLibrary.blosc_init();
+		int size = BloscLibrary.blosc_compress(5, 1, new NativeLong(4), new NativeLong(isize),
+				m.getByteBuffer(0, isize), m2.getByteBuffer(0, isize), new NativeLong(isize));
+		data_out = m2.getFloatArray(0, SIZE);
+		Memory m3 = new Memory(isize);
+		BloscLibrary.blosc_decompress(m2.getByteBuffer(0, isize), m3.getByteBuffer(0, isize), new NativeLong(isize));
+		float[] data_in = m3.getFloatArray(0, SIZE);
+		assertArrayEquals(data, data_in, (float) 0);
+		BloscLibrary.blosc_destroy();
+		assertNotNull(data_out);
+		assertTrue(size < isize);
+	}
+
+	@Test
+	public void testSetCompressor() {
+		System.out.println("*** testSetCompressor ***");
+		int SIZE = 26214400;
+		char data[] = new char[SIZE];
+		for (int i = 0; i < SIZE; i++) {
+			// data[i] = Math.random();
+			data[i] = (char) i;
+		}
+		ByteBuffer b = Util.array2ByteBuffer(data);
+		BloscWrapper bw = new BloscWrapper();
+		bw.init();
+		System.out.println("Blosc version " + bw.getVersionString());
+		bw.setNumThreads(4);
+		System.out.println("Working with " + bw.getNumThreads() + " threads");
+		assertEquals(bw.getNumThreads(), 4);
+		String compnames = bw.listCompressors();
+		String compnames_array[] = compnames.split(",");
+		for (String compname : compnames_array) {
+			bw.setCompressor(compname);
+			String compname_out = bw.getCompressor();
+			assertEquals(compname, compname_out);
+			String[] ci = bw.getComplibInfo(compname);
+			int compcode = bw.compnameToCompcode(compname);
+			compname_out = bw.compcodeToCompname(compcode);
+			assertEquals(compname, compname_out);
+			System.out
+					.println("Working with compressor " + compname + " (code " + compcode + ") " + ci[0] + " " + ci[1]);
+			long startTime = System.currentTimeMillis();
+			ByteBuffer o = ByteBuffer.allocateDirect(SIZE * 2 + BloscWrapper.OVERHEAD);
+			// int s = bw.compressCtx(5, Shuffle.BYTE_SHUFFLE,
+			// PrimitiveSizes.DOUBLE_FIELD_SIZE, b, SIZE * 8, o,
+			// SIZE * 8 + BloscWrapper.OVERHEAD, compname, 0, 1);
+			bw.compress(5, Shuffle.BYTE_SHUFFLE, PrimitiveSizes.CHAR_FIELD_SIZE, b,
+					SIZE * PrimitiveSizes.CHAR_FIELD_SIZE, o,
+					SIZE * PrimitiveSizes.CHAR_FIELD_SIZE + BloscWrapper.OVERHEAD);
+			long stopTime = System.currentTimeMillis();
+			long elapsedTime = stopTime - startTime;
+			bw.cbufferComplib(o);
+			// System.out.println("Complib " + complib.array());
+			IntBuffer version = IntBuffer.allocate(1);
+			IntBuffer versionlz = IntBuffer.allocate(1);
+			bw.cbufferVersions(o, version, versionlz);
+			System.out.println("Versions " + version.get(0) + ", " + versionlz.get(0));
+			NativeLongByReference typesize = new NativeLongByReference();
+			IntBuffer flags = IntBuffer.allocate(1);
+			bw.cbufferMetainfo(o, typesize, flags);
+			System.out.println("Metainfo " + typesize.getValue() + ", " + flags.get(0));
+			printRatio(bw, "Char Array", o);
+			BufferSizes bs = bw.cbufferSizes(o);
+			double mb = bs.getNbytes() * 1.0 / (1024 * 1024);
+			System.out.println("Compress time " + elapsedTime + " ms. "
+					+ String.format("%.2f", (mb / elapsedTime) * 1000) + " Mb/s");
+			startTime = System.currentTimeMillis();
+			ByteBuffer a = ByteBuffer.allocateDirect(SIZE * 2);
+			bw.decompress(o, a, SIZE * 2);
+			stopTime = System.currentTimeMillis();
+			elapsedTime = stopTime - startTime;
+			mb = (bs.getNbytes() * 1.0) / (1024 * 1024);
+			char[] data_again = Util.byteBufferToCharArray(a);
+			System.out.println("Decompress time " + elapsedTime + " ms. "
+					+ String.format("%.2f", (mb / elapsedTime) * 1000) + " Mb/s");
+			assertArrayEquals(data, data_again);
+		}
+		bw.setBlocksize(256 * 1024);
+		int bs = bw.getBlocksize();
+		assertTrue(256 * 1024 == bs);
+		bw.freeResources();
+		bw.destroy();
+	}
+
+	private void printRatio(BloscWrapper bw, String title, ByteBuffer cbuffer) {
+		BufferSizes bs = bw.cbufferSizes(cbuffer);
+		System.out.println(title + ": " + bs.getCbytes() + " from " + bs.getNbytes() + ". Ratio: "
+				+ (String.format("%.2f", (0.0 + bs.getNbytes()) / bs.getCbytes())));
+	}
+
+	@Test
+	public void testCompressDecompressDouble() {
+		int SIZE = 100 * 100 * 100;
+		double data[] = new double[SIZE];
+		for (int i = 0; i < SIZE; i++) {
+			data[i] = i * 2;
+		}
+		ByteBuffer ibb = Util.array2ByteBuffer(data);
+		BloscWrapper bw = new BloscWrapper();
+		bw.init();
+		ByteBuffer obb = ByteBuffer.allocateDirect(ibb.limit() + BloscWrapper.OVERHEAD);
+		bw.compress(5, Shuffle.BYTE_SHUFFLE, PrimitiveSizes.DOUBLE_FIELD_SIZE, ibb, ibb.limit(), obb, obb.limit());
+		printRatio(bw, "Double", obb);
+		ByteBuffer abb = ByteBuffer.allocateDirect(ibb.limit());
+		bw.decompress(obb, abb, abb.limit());
+		double[] data_again = Util.byteBufferToDoubleArray(abb);
+		bw.destroy();
+		assertArrayEquals(data, data_again, (float) 0);
+	}
+
+	@Test
+	public void testCompressDecompressFloat() {
+		int SIZE = 100 * 100 * 100;
+		float data[] = new float[SIZE];
+		for (int i = 0; i < SIZE; i++) {
+			data[i] = i * 2;
+		}
+		ByteBuffer ibb = Util.array2ByteBuffer(data);
+		BloscWrapper bw = new BloscWrapper();
+		bw.init();
+		ByteBuffer obb = ByteBuffer.allocateDirect(ibb.limit() + BloscWrapper.OVERHEAD);
+		bw.compress(5, Shuffle.BYTE_SHUFFLE, PrimitiveSizes.FLOAT_FIELD_SIZE, ibb, ibb.limit(), obb, obb.limit());
+		printRatio(bw, "Float", obb);
+		ByteBuffer abb = ByteBuffer.allocateDirect(ibb.limit());
+		bw.decompress(obb, abb, abb.limit());
+		float[] data_again = Util.byteBufferToFloatArray(abb);
+		bw.destroy();
+		assertArrayEquals(data, data_again, (float) 0);
+	}
+
+	@Test
+	public void testCompressDecompressLong() {
+		int SIZE = 100 * 100 * 100;
+		long data[] = new long[SIZE];
+		for (int i = 0; i < SIZE; i++) {
+			data[i] = i * 2;
+		}
+		ByteBuffer ibb = Util.array2ByteBuffer(data);
+		BloscWrapper bw = new BloscWrapper();
+		bw.init();
+		ByteBuffer obb = ByteBuffer.allocateDirect(ibb.limit() + BloscWrapper.OVERHEAD);
+		bw.compress(5, Shuffle.BYTE_SHUFFLE, PrimitiveSizes.LONG_FIELD_SIZE, ibb, ibb.limit(), obb, obb.limit());
+		printRatio(bw, "Long", obb);
+		ByteBuffer abb = ByteBuffer.allocateDirect(ibb.limit());
+		bw.decompress(obb, abb, abb.limit());
+		long[] data_again = Util.byteBufferToLongArray(abb);
+		bw.destroy();
+		assertArrayEquals(data, data_again);
+	}
+
+	@Test
+	public void testCompressDecompressInt() {
+		int SIZE = 100 * 100 * 100;
+		int data[] = new int[SIZE];
+		for (int i = 0; i < SIZE; i++) {
+			data[i] = i * 2;
+		}
+		ByteBuffer ibb = Util.array2ByteBuffer(data);
+		BloscWrapper bw = new BloscWrapper();
+		bw.init();
+		ByteBuffer obb = ByteBuffer.allocateDirect(ibb.limit() + BloscWrapper.OVERHEAD);
+		bw.compress(5, Shuffle.BYTE_SHUFFLE, PrimitiveSizes.INT_FIELD_SIZE, ibb, ibb.limit(), obb, obb.limit());
+		printRatio(bw, "Int", obb);
+		ByteBuffer abb = ByteBuffer.allocateDirect(ibb.limit());
+		bw.decompress(obb, abb, abb.limit());
+		int[] data_again = Util.byteBufferToIntArray(abb);
+		bw.destroy();
+		assertArrayEquals(data, data_again);
+	}
+
+	@Test
+	public void testCompressDecompressDoubleCtx() {
+		int SIZE = 100 * 100 * 100;
+		double data[] = new double[SIZE];
+		for (int i = 0; i < SIZE; i++) {
+			data[i] = i;
+		}
+		ByteBuffer ibb = Util.array2ByteBuffer(data);
+		BloscWrapper bw = new BloscWrapper();
+		bw.init();
+		ByteBuffer obb = ByteBuffer.allocateDirect(ibb.limit() + BloscWrapper.OVERHEAD);
+		ContextCparams cctx = new ContextCparams();
+		cctx.clevel = 5;
+		cctx.filtercode = BloscLibrary.BLOSC_DOSHUFFLE;
+		cctx.compcode = BloscLibrary.BLOSC_LZ4;
+		cctx.typesize = PrimitiveSizes.DOUBLE_FIELD_SIZE;
+		cctx.nthreads = 2;
+		// cctx.blocksize = 256 * 1024;
+		// cctx.nthreads = 1;
+		bw.compressCtx(cctx, ibb, ibb.limit(), obb, obb.limit());
+		printRatio(bw, "Double", obb);
+		ByteBuffer abb = ByteBuffer.allocateDirect(ibb.limit());
+		ContextDparams dctx = new ContextDparams();
+		ByteBuffer subsetbb = ByteBuffer.allocateDirect(5 * PrimitiveSizes.DOUBLE_FIELD_SIZE);
+		int ret = bw.getitemCtx(dctx, obb, 5, 5, subsetbb);
+		assertTrue(ret >= 0);
+		double data_subset_ref[] = { 5, 6, 7, 8, 9 };
+		for (int i = 0; i < 5; i++) {
+			assertTrue(subsetbb.getDouble(i * PrimitiveSizes.DOUBLE_FIELD_SIZE) == data_subset_ref[i]);
+		}
+		dctx.nthreads = 2;
+		bw.decompressCtx(dctx, obb, abb, abb.limit());
+		double[] data_again = Util.byteBufferToDoubleArray(abb);
+		bw.freeCtx(cctx);
+		bw.freeCtx(dctx);
+		bw.destroy();
+		assertArrayEquals(data, data_again, (float) 0);
+	}
+
+	@Test
+	public void testCompressDecompressDirectBuffer() {
+		int SIZE = 100 * 100 * 100;
+		ByteBuffer ibb = ByteBuffer.allocateDirect(SIZE * PrimitiveSizes.DOUBLE_FIELD_SIZE);
+		for (int i = 0; i < SIZE; i++) {
+			ibb.putDouble(i);
+		}
+		BloscWrapper bw = new BloscWrapper();
+		bw.init();
+		ByteBuffer obb = ByteBuffer.allocateDirect(ibb.limit() + BloscWrapper.OVERHEAD);
+		bw.compress(5, Shuffle.BYTE_SHUFFLE, PrimitiveSizes.DOUBLE_FIELD_SIZE, ibb, ibb.limit(), obb, obb.limit());
+		printRatio(bw, "Double", obb);
+		ByteBuffer abb = ByteBuffer.allocateDirect(ibb.limit());
+		bw.decompress(obb, abb, abb.limit());
+		bw.destroy();
+		assertEquals(ibb, abb);
+	}
+
+	public void testChunks() {
+		int SIZE = 100 * 100 * 100;
+		double data[] = new double[SIZE];
+		for (int i = 0; i < SIZE; i++) {
+			data[i] = i;
+		}
+		ByteBuffer ibb = Util.array2ByteBuffer(data);
+		BloscWrapper bw = new BloscWrapper();
+		bw.init();
+		int iBufferSize = SIZE * PrimitiveSizes.DOUBLE_FIELD_SIZE;
+		int oBufferSize = SIZE * PrimitiveSizes.DOUBLE_FIELD_SIZE + BloscWrapper.OVERHEAD;
+		ByteBuffer obb = ByteBuffer.allocateDirect(oBufferSize);
+		int w = bw.compress(5, 1, PrimitiveSizes.DOUBLE_FIELD_SIZE, ibb, iBufferSize, obb, oBufferSize);
+
+		Sparams sparams = new Sparams();
+		sparams.filters[0] = BloscLibrary.BLOSC_DELTA;
+		sparams.filters[1] = BloscLibrary.BLOSC_SHUFFLE;
+		Sheader sheader = bw.newSchunk(sparams);
+		/* Now append a couple of chunks */
+		int nchunks = bw.appendBuffer(sheader, PrimitiveSizes.DOUBLE_FIELD_SIZE, iBufferSize, ibb);
+		assertEquals(nchunks, 1);
+		nchunks = bw.appendBuffer(sheader, PrimitiveSizes.DOUBLE_FIELD_SIZE, iBufferSize, ibb);
+		assertEquals(nchunks, 2);
+
+		/* Retrieve and decompress the chunks (0-based count) */
+		int dsize = bw.decompressChunk(sheader, 0, obb, iBufferSize);
+		assertTrue(dsize > 0);
+		dsize = bw.decompressChunk(sheader, 1, obb, iBufferSize);
+		assertTrue(dsize > 0);
+
+		for (int i = 0; i < SIZE; i++) {
+			if (ibb.getDouble(i) != obb.getDouble(i)) {
+				// printf("i, values: %d, %f, %f\n", i, data[i], data_dest[i]);
+				System.out.println("Values " + ibb.getDouble(i) + ", " + obb.getDouble(i));
+				System.out.println("Decompressed data differs from original at " + i + "!");
+				fail("Decompressed data differs from original at " + i + "!");
+			}
+		}
+
+		ByteBuffer abb = ByteBuffer.allocateDirect(iBufferSize);
+		bw.decompress(obb, abb, iBufferSize);
+
+		bw.destroySchunk(sheader);
+		bw.destroy();
+	}
+
+	public void testDeltaPackedChunks() {
+		int SIZE = 100 * 100 * 100;
+		double data[] = new double[SIZE];
+		for (int i = 0; i < SIZE; i++) {
+			data[i] = i;
+		}
+		ByteBuffer ibb = Util.array2ByteBuffer(data);
+		BloscWrapper bw = new BloscWrapper();
+		bw.init();
+		int iBufferSize = SIZE * PrimitiveSizes.DOUBLE_FIELD_SIZE;
+		int oBufferSize = SIZE * PrimitiveSizes.DOUBLE_FIELD_SIZE + BloscWrapper.OVERHEAD;
+		ByteBuffer obb = ByteBuffer.allocateDirect(oBufferSize);
+
+		Sparams sparams = new Sparams();
+		sparams.filters[0] = BloscLibrary.BLOSC_DELTA;
+		sparams.filters[1] = BloscLibrary.BLOSC_SHUFFLE;
+		Sheader sheader = bw.newSchunk(sparams);
+
+		int nchunks = bw.appendBuffer(sheader, PrimitiveSizes.DOUBLE_FIELD_SIZE, iBufferSize, ibb);
+		assertTrue(nchunks == 1);
+
+		assertTrue(sheader.nbytes > 0);
+		assertTrue(sheader.cbytes > 0);
+
+		ByteBuffer packed = (ByteBuffer) bw.packSchunk(sheader);
+		Sheader sheader2 = bw.unpackSchunk(packed);
+		sheader2.clear();
+
+		/* Now append another chunk (essentially the same as the reference) */
+		packed = (ByteBuffer) bw.packedAppendBuffer(packed, PrimitiveSizes.DOUBLE_FIELD_SIZE, iBufferSize, ibb);
+
+		long dsize = bw.packedDecompressChunk(packed, 1, obb);
+		assertTrue(dsize > 0);
+
+		for (int i = 0; i < SIZE; i++) {
+			if (ibb.getDouble(i) != obb.getDouble(i)) {
+				System.out.println("Values " + ibb.getDouble(i) + ", " + obb.getDouble(i));
+				System.out.println("Decompressed data differs from original at " + i + "!");
+				fail("Decompressed data differs from original at " + i + "!");
+			}
+		}
+
+		bw.destroySchunk(sheader);
+		bw.destroy();
+	}
+}
